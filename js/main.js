56839b73b217c760ab2a3c58dfbee03f7c92f59a
ahmeddoban <ahmedzoro323@gmail.com>
2022-09-03 01:20:31 +0200

    start new project

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let check = document.querySelector(".check");
+let nav = document.querySelector(".main-nav");
+let menue_icon = document.querySelector(".menue_icon");
+let header = document.querySelector(".header");
+check.addEventListener("click", (ele) => {
+  if (check.checked) {
+    nav.style.cssText = "right:0";
+    menue_icon.classList.add("active", "open");
+  } else {
+    nav.style.cssText = "right:-100%";
+    menue_icon.classList.remove("active", "open");
+  }
+});
+window.onscroll = () => {
+  if (window.scrollY >= 550) {
+    header.style.cssText = "background-color : #000";
+  } else {
+    header.style.cssText = "background-color : transparent";
+  }
+};
