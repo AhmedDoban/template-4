77834c2d35d6453ba3f58ad7eed894056232a62d
ahmeddoban <ahmedzoro323@gmail.com>
2022-09-04 20:04:21 +0200

    stats

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/*ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+navbar  menu
+ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ*/
 let check = document.querySelector(".check");
 let nav = document.querySelector(".main-nav");
 let menue_icon = document.querySelector(".menue_icon");
@@ -18,3 +21,18 @@ window.onscroll = () => {
     header.style.cssText = "background-color : transparent";
   }
 };
+/*ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+stats
+ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ*/
+let stats_number = document.querySelectorAll(".stats-number");
+let number = [...stats_number];
+for (let i = 0; i < number.length; i++) {
+  let data = +number[i].innerHTML;
+  let counter = setInterval(function stats() {
+    number[i].innerHTML = data;
+    data += 1;
+    if (data == number[i].dataset.count) {
+      clearInterval(counter);
+    }
+  }, 1);
+}
